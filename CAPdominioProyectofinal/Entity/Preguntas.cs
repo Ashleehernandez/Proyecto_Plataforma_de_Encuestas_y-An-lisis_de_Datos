b2e2d743f0e26956f305304fbff414eb56081830
Juan Pablo Canturriano Upia <ing.juan.canturriano@gmail.com>
2025-04-18 14:50:30 -0400

    Updare

--- CAPdominioProyectofinal/Entity/Preguntas.cs
+++ CAPdominioProyectofinal/Entity/Preguntas.cs
@@ -3,21 +3,18 @@
 public class Preguntas
 {
     public int Id { get; set; }
-    public string Texto { get; set; }
+    public string? Texto { get; set; }
     public TipoPregunta TipoPregunta { get; set; }
     public int EncuestaId { get; set; }
-    //public int Opciones { get; set; };
-    //public int Min { get; set; } = 1;
-    //public int Max { get; set; } = 100;
-    // Para preguntas de opción múltiple
-    public virtual ICollection<Opciones> Opciones { get; set; }
+
+    public virtual ICollection<Opciones>? Opciones { get; set; }
 
     // Para preguntas de escala
     public int? EscalaMin { get; set; }
     public int? EscalaMax { get; set; }
 
     // Relaciones
-    public virtual Encuesta Encuesta { get; set; }
+    public virtual Encuesta? Encuesta { get; set; }
     public virtual ICollection<Respuestas> Respuestas { get; set; }
     public int UsuarioId { get; set; }
 }
